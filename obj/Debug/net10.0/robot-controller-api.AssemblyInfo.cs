22e24b9c1b06e5cc2b507e3cf60bc0ea08de4cb3
hemant-8 <hemantsachdeva19@gmail.com>
2026-04-19 14:49:58 +0530

    final clean structure

--- obj/Debug/net10.0/robot-controller-api.AssemblyInfo.cs
+++ obj/Debug/net10.0/robot-controller-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("robot-controller-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8b39b401a17cd959ec3d0b4c250d0a8963fdabc")]
 [assembly: System.Reflection.AssemblyProductAttribute("robot-controller-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("robot-controller-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
